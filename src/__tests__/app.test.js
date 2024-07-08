2fb8e87dff6383bbac633d923347147c90ef5a82
Ankur Garg <ankurg@umbrellainfocare.com>
2024-07-08 16:01:13 +0530

    test case for checking snahpshot of app component. moving app component code to login

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import App from "../app"
 
+afterEach(cleanup);
+
 test('renders learn react link', () => {
-  render(<App/>);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it('it should take snap', () => {
+  const { asFragment } = render(<App />)
+  expect(asFragment(<App />)).toMatchSnapshot();
+})
