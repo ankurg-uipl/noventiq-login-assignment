ae6e652e813db47414de4da030a301605d6e681d
Ankur Garg <ankurg@umbrellainfocare.com>
2024-07-09 20:02:08 +0530

    updating test case after implementing react-intl as intl provider object is need to execute test cases

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { cleanup, fireEvent, render } from "@testing-library/react"
 import Login from "../lib/components/login/login";
+import LanguageWrapper from "../lib/i18n/language-wrapper";
 
 
 afterEach(cleanup);
 
+const renderWithReactIntl = (component) => {
+    return render(<LanguageWrapper>
+                   {component}
+                  </LanguageWrapper>
+    );
+  };
+
 describe("Test Email Fields", () => {
     test('Email Input should exist', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputemail');
         expect(element).toBeInTheDocument();
     })
 
     test('Label Email should exist', () => {
-        const { getByLabelText } = render(<Login />);
+        const { getByLabelText } = renderWithReactIntl(<Login />);
         expect(getByLabelText('Email')).toBeInTheDocument();
     })
 
     test('Email Input value should change', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputemail');
         fireEvent.change(element, { target: { value: 'dummy@gmail.com' } });
         expect(element.value).toBe('dummy@gmail.com');
@@ -26,22 +34,22 @@ describe("Test Email Fields", () => {
 
 
     test('Should show Required Message', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputemail');
         fireEvent.blur(element);
-        expect(getByTestId("erroremail")).toHaveTextContent("Email Address is required");
+        expect(getByTestId("erroremail")).toHaveTextContent("Email is required");
     })
 
     test('Should show invalid Email', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputemail');
         fireEvent.change(element, { target: { value: 'dummy@gmail.com' } });
         fireEvent.blur(element);
-        expect(getByTestId("erroremail")).toHaveTextContent("Invalid Email Address");
+        expect(getByTestId("erroremail")).toHaveTextContent("Invalid Email");
     })
 
     test('Should not show invalid Email', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputemail');
         fireEvent.change(element, { target: { value: 'a@brownsugar.com' } });
         fireEvent.blur(element);
@@ -51,25 +59,25 @@ describe("Test Email Fields", () => {
 
 describe("Test Password Field", () => {
     test('Password Input should exist', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputpassword');
         expect(element).toBeInTheDocument();
     })
 
     test('Label Password should exist', () => {
-        const { getByLabelText } = render(<Login />);
+        const { getByLabelText } = renderWithReactIntl(<Login />);
         expect(getByLabelText('Password')).toBeInTheDocument();
     })
 
     test('Password Input value should change', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputpassword');
         fireEvent.change(element, { target: { value: '12345' } });
         expect(element.value).toBe('12345');
     })
 
     test('Should show Required Message', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputpassword');
         fireEvent.blur(element);
         expect(getByTestId("errorpassword")).toHaveTextContent("Password is required");
@@ -77,7 +85,7 @@ describe("Test Password Field", () => {
 
 
     test('Should show Temporary Password', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputpassword');
         const btn = getByTestId('showpassword');
         fireEvent.click(btn);
@@ -89,30 +97,30 @@ describe("Test Password Field", () => {
 
 describe("Test Language Field", () => {
     test('Language Selection should exist', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputlanguage');
         expect(element).toBeInTheDocument();
     })
 
     test('Label Language should exist', () => {
-        const { getByLabelText } = render(<Login />);
+        const { getByLabelText } = renderWithReactIntl(<Login />);
         expect(getByLabelText('Language')).toBeInTheDocument();
     })
 
-    test('Should have 2 Options', () => {
-        const { getAllByTestId } = render(<Login />);
+    test('Should have 3 Options', () => {
+        const { getAllByTestId } = renderWithReactIntl(<Login />);
         const options = getAllByTestId('select-option');
-        expect(options).toHaveLength(2);
+        expect(options).toHaveLength(3);
     })
 
     test('Should Match Default Language', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputlanguage');
         expect(element.value).toBe('en');
     })
 
     test('Should Change to Hindi Option', () => {
-        const { getByTestId, getAllByTestId } = render(<Login />);
+        const { getByTestId, getAllByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputlanguage');
         const options = getAllByTestId('select-option');
         fireEvent.change(element, { target: { value: 'hi' } });
@@ -125,31 +133,31 @@ describe("Test Language Field", () => {
 
 describe("Test Remember me Field", () => {
     test('Remember Switch should exist', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputremember');
         expect(element).toBeInTheDocument();
     })
 
     test('Label Remember me should exist', () => {
-        const { getByLabelText } = render(<Login />);
+        const { getByLabelText } = renderWithReactIntl(<Login />);
         expect(getByLabelText('Remember me')).toBeInTheDocument();
     })
 
     test('Default value should be empty or falsy ', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputremember');
         expect(element.checked).toBeFalsy();
     })
 
     test('Should change the value on checkbok click ', () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputremember');
         fireEvent.click(element)
         expect(element.checked).toBeTruthy();
     })
 
     test('Should change the value on label click', () => {
-        const { getByTestId, getByLabelText } = render(<Login />);
+        const { getByTestId, getByLabelText } = renderWithReactIntl(<Login />);
         const element = getByTestId('inputremember');
         fireEvent.click(getByLabelText('Remember me'))
         expect(element.checked).toBeTruthy();
@@ -159,21 +167,21 @@ describe("Test Remember me Field", () => {
 
 describe("Test Submit Button", () => {
     test("Submit Button Should Exist", () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         expect(getByTestId('submitBtn')).toBeInTheDocument();
     })
 
     test("Should show errors on input fields when submit form ", () => {
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const btn = getByTestId('submitBtn');
         fireEvent.click(btn);
-        expect(getByTestId('erroremail')).toHaveTextContent('Email Address is required');
+        expect(getByTestId('erroremail')).toHaveTextContent('Email is required');
         expect(getByTestId('errorpassword')).toHaveTextContent('Password is required');
     })
 
     test("Should show success message when submit form", () => {
         const logSpy = jest.spyOn(console, 'log');
-        const { getByTestId } = render(<Login />);
+        const { getByTestId } = renderWithReactIntl(<Login />);
         const password = getByTestId('inputpassword');
         fireEvent.change(password, { target: { value: '12345' } });
         const email = getByTestId('inputemail');
