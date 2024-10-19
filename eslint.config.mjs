2d626cd99c20240546e3d37b056cdcdde79e3de2
ankug-uipl <ankurg@umbrellainfocare.com>
2024-10-19 19:18:39 +0530

    installing stylistic/eslint-plugin-js and unused-import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
 import pluginJs from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
 import pluginImort from "eslint-plugin-perfectionist";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
+import unusedImports from "eslint-plugin-unused-imports";
 import globals from "globals";
+
+// https://github.com/promptfoo/promptfoo/blob/main/eslint.config.mjs
+// https://github.com/nodejs/node/blob/main/eslint.config.mjs
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
@@ -19,10 +24,34 @@ export default [
   pluginImort.configs["recommended-alphabetical"],
   {
     plugins: {
+      '@stylistic/js': stylisticJs,
       "react-hooks": pluginReactHooks,
+      "unused-imports": unusedImports
     },
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "spaced-comment": ["error", "always", {
+    "block": {
+        "balanced": true,
+        "exceptions": ["*"],
+        "markers": ["!"]
+    },
+    "line": {
+        "exceptions": ["-", "+"],
+        "markers": ["/"]
+    }
+}],
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+            "warn",
+            {
+                "args": "after-used",
+                "argsIgnorePattern": "^_",
+                "vars": "all",
+                "varsIgnorePattern": "^_",
+            },
+        ]
     },
   }
 ];
